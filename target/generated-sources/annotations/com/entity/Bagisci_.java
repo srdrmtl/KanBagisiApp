df5d631d1b1acf5c111d6bfaf1fda5d1366a1db7
serdar <serdar@serdar-K55VJ>
2018-04-17 20:49:26 +0300

    Commit 2

---
+++ target/generated-sources/annotations/com/entity/Bagisci_.java
@@ -0,0 +1,21 @@
+package com.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-17T16:37:40")
+@StaticMetamodel(Bagisci.class)
+public class Bagisci_ { 
+
+    public static volatile SingularAttribute<Bagisci, String> password;
+    public static volatile SingularAttribute<Bagisci, Boolean> hide;
+    public static volatile SingularAttribute<Bagisci, String> ad;
+    public static volatile SingularAttribute<Bagisci, String> mail;
+    public static volatile SingularAttribute<Bagisci, String> soyad;
+    public static volatile SingularAttribute<Bagisci, String> kanGrubu;
+    public static volatile SingularAttribute<Bagisci, Integer> id;
+    public static volatile SingularAttribute<Bagisci, String> adres;
+    public static volatile SingularAttribute<Bagisci, String> sehir;
+
+}
